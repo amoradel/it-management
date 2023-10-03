f31f7f29eac4e9826d2d998ed459e829c065d0d2
EminRams <eminramos16@gmail.com>
2023-10-03 09:02:36 -0600

    style(All): :art: traduccion del label Activities en computadoras y piezas/equipos

--- app/Filament/Resources/ComputerResource.php
+++ app/Filament/Resources/ComputerResource.php
@@ -274,7 +274,8 @@ class ComputerResource extends Resource
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
                 Tables\Actions\Action::make('activities')->url(fn ($record) => ComputerResource::getUrl('activities', ['record' => $record]))
-                    ->icon('heroicon-m-information-circle'),
+                    ->icon('heroicon-m-information-circle')
+                    ->translateLabel(),
                 Tables\Actions\ForceDeleteAction::make(),
                 Tables\Actions\RestoreAction::make(),
             ])
