27e509e1c395489e39109316b5ee14854cf29c79
Anibal Moradel <amoradel@outlook.com>
2023-11-23 13:37:57 -0600

    feat(ExportImport): :sparkles: Import brands

--- app/Filament/Resources/BrandResource/Pages/ListBrands.php
+++ app/Filament/Resources/BrandResource/Pages/ListBrands.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\BrandResource\Pages;
 use App\Filament\Resources\BrandResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
 
 class ListBrands extends ListRecords
 {
@@ -14,6 +16,14 @@ class ListBrands extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ImportAction::make()
+                ->icon('heroicon-o-document-arrow-up')
+                ->modalWidth('md')
+                ->uniqueField('name')
+                ->fields([
+                    ImportField::make('name')
+                        ->required(),
+                ]),
         ];
     }
 }
