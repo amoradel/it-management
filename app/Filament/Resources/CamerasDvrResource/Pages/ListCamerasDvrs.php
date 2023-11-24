c2f452c301fb107cdbd5675d251ef195f3befd54
EminRams <eminramos16@gmail.com>
2023-11-24 08:08:36 -0600

    feat: :construction: work in progress

--- app/Filament/Resources/CamerasDvrResource/Pages/ListCamerasDvrs.php
+++ app/Filament/Resources/CamerasDvrResource/Pages/ListCamerasDvrs.php
@@ -3,8 +3,16 @@
 namespace App\Filament\Resources\CamerasDvrResource\Pages;
 
 use App\Filament\Resources\CamerasDvrResource;
+use App\Models\Device;
+use App\Filament\Resources\BrandResource;
+use App\Filament\Resources\ModelResource;
+use App\Filament\Resources\TypeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
+use PhpParser\Node\Stmt\Label;
+use Illuminate\Support\Carbon;
 
 class ListCamerasDvrs extends ListRecords
 {
@@ -14,6 +22,81 @@ class ListCamerasDvrs extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ImportAction::make()
+                ->icon('heroicon-o-document-arrow-up')
+                ->modalWidth('md')
+                ->uniqueField('name')
+                ->fields([
+                    ImportField::make('name')
+                        ->required(),
+                    ImportField::make('location')
+                        ->required(),
+                    ImportField::make('brand.name')
+                        ->label('Brand name')
+                        ->required(),
+                    ImportField::make('model.name')
+                        ->label('Model name')
+                        ->required(),
+                    ImportField::make('type.name')
+                        ->label('Type name')
+                        ->required(),
+                    ImportField::make('device_type')
+                        ->required(),
+                    ImportField::make('asset_number')
+                        ->required(),
+                    ImportField::make('serial_number')
+                        ->required(),
+                    ImportField::make('condition')
+                        ->required(),
+                    ImportField::make('entry_date')
+                        ->required(),
+                    ImportField::make('observation')
+                        ->required(),
+                    ImportField::make('status')
+                        ->required(),
+                ])->handleRecordCreation(function (array $data) {
+                    $options = [
+                        'camera',
+                        'dvr',
+                        'IVMS-4200',
+                        'CMS3.0',
+                        'VI MonitorPlus',
+                        'Viejo',
+                        'Nuevo',
+                    ];
+
+                    $brand = BrandResource::getEloquentQuery()->where('name', $data['brand']['name'])->first();
+                    $model = ModelResource::getEloquentQuery()->where('name', $data['model']['name'])->first();
+                    $type = TypeResource::getEloquentQuery()->where('name', $data['type']['name'])->first();
+
+                    if ($brand && $model && $type) {
+                        $documentData = [
+                            'name' => $data['name'],
+                            'brand_id' => $brand->id,
+                            'model_id' => $model->id,
+                            'type_id' => $type->id,
+                            'device_type' => $data['device_type'],
+                            'condition' => $data['condition'],
+                            'location' => $data['location'],
+                            'asset_number' => $data['asset_number'],
+                            'serial_number' => $data['serial_number'],
+                            'entry_date' => Carbon::createFromFormat('Y-m-d', '1900-01-01')->addDays($data['entry_date'] - 2),
+                            // 'status' => $data['status'],
+                        ];
+
+                        if (isset($data['observation'])) {
+                            $documentData['observation'] = $data['observation'];
+                        }
+
+                        if (isset($data['dvr_program'])) {
+                            $documentData['dvr_program'] = $data['dvr_program'];
+                        }
+
+                        return Device::create($documentData);
+                    }
+                    // AUN NO FUNCIONA LA VALIDACION DE DATOS
+                    return new Device();
+                }),
         ];
     }
 }
--- app/Filament/Resources/TypeResource/Pages/ListTypes.php
+++ app/Filament/Resources/TypeResource/Pages/ListTypes.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\TypeResource\Pages;
 
 use App\Filament\Resources\TypeResource;
+use App\Models\Type;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
 
 class ListTypes extends ListRecords
 {
@@ -12,8 +15,54 @@ class ListTypes extends ListRecords
 
     protected function getHeaderActions(): array
     {
+
         return [
             Actions\CreateAction::make(),
+            ImportAction::make()
+                ->icon('heroicon-o-document-arrow-up')
+                ->modalWidth('md')
+                ->uniqueField('name')
+                ->fields([
+                    ImportField::make('device_type')
+                        ->label('Device name')
+                        ->required(),
+                    ImportField::make('name')
+                        ->required(),
+                    ImportField::make('description'),
+                ])->handleRecordCreation(function (array $data) {
+                    $options = [
+                        'computer',
+                        'printer',
+                        'camera',
+                        'monitor',
+                        'pos',
+                        'dvr',
+                        'others',
+                        'Computadora',
+                        'Impresora',
+                        'Camara',
+                        'Monitor',
+                        'Pos',
+                        'Dvr',
+                        'Accesorios y Otros',
+                    ];
+
+                    if (in_array($data['device_type'], $options)) {
+                        $documentData = [
+                            'device_type' => $data['device_type'],
+                            'name' => $data['name'],
+                        ];
+    
+                        if (isset($data['description'])) {
+                            $documentData['description'] = $data['description'];
+                        }
+    
+                        return Type::create($documentData);
+                    }
+
+
+                    return new Type();
+                }),
         ];
     }
 }
