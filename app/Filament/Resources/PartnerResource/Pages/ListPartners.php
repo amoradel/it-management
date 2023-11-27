336b627dbdffc27d44c83f675663403efd07b3ff
EminRams <eminramos16@gmail.com>
2023-11-27 11:33:23 -0600

    feat(Partners): :sparkles: Feature para exportar datos en un archivo de excel

--- app/Filament/Resources/PartnerResource/Pages/ListPartners.php
+++ app/Filament/Resources/PartnerResource/Pages/ListPartners.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\PartnerResource\Pages;
 use App\Filament\Resources\PartnerResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Columns\Column;
 
 class ListPartners extends ListRecords
 {
@@ -14,6 +17,18 @@ class ListPartners extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+
+            ExportAction::make()
+            ->exports([
+                ExcelExport::make()
+                    ->fromTable()
+                    ->withFilename("Empleados" . '-' . date('Y-m-d'))
+                    ->withWriterType(\Maatwebsite\Excel\Excel::XLSX)
+                    ->withColumns([
+                        Column::make('email')->heading('Correo Electrónico'),
+                        Column::make('company_position')->heading('Cargo'),
+                    ]),
+            ]),
         ];
     }
 }
