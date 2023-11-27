29fea05cf477023a49ef97c2448f3b4f5fc36dcc
EminRams <eminramos16@gmail.com>
2023-11-27 11:36:53 -0600

    feat(POS): :sparkles: Feature para exportar datos en un archivo de excel

--- app/Filament/Resources/POSResource/Pages/ListPOS.php
+++ app/Filament/Resources/POSResource/Pages/ListPOS.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\POSResource\Pages;
 use App\Filament\Resources\POSResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Columns\Column;
 
 class ListPOS extends ListRecords
 {
@@ -14,6 +17,19 @@ class ListPOS extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+
+            ExportAction::make()
+                ->exports([
+                    ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename("Pos" . '-' . date('Y-m-d'))
+                        ->withWriterType(\Maatwebsite\Excel\Excel::XLSX)
+                        ->withColumns([
+                            Column::make('condition')->heading('Condición'),
+                            Column::make('entry_date')->heading('Fecha de Ingreso'),
+                            Column::make('observation')->heading('Observación'),
+                        ]),
+                ]),
         ];
     }
 }
