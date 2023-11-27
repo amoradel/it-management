11a1b046053824ff773acb8619415a5ce8508009
EminRams <eminramos16@gmail.com>
2023-11-27 10:16:30 -0600

    feat(Cameras_DVR's): :sparkles: Feature para exportar datos en un archivo de excel

--- app/Filament/Resources/CamerasDvrResource/Pages/ListCamerasDvrs.php
+++ app/Filament/Resources/CamerasDvrResource/Pages/ListCamerasDvrs.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\CamerasDvrResource\Pages;
 use App\Filament\Resources\CamerasDvrResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListCamerasDvrs extends ListRecords
 {
@@ -14,6 +16,14 @@ class ListCamerasDvrs extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            
+            ExportAction::make()
+            ->exports([
+                ExcelExport::make()
+                    ->fromTable()
+                    ->withFilename("Camaras-Dvr's" . '-' . date('Y-m-d'))
+                    ->withWriterType(\Maatwebsite\Excel\Excel::XLSX),
+            ]),
         ];
     }
 }
