a9dba6ba84b3e6db83199aa1e3f511fa5d7ebf92
EminRams <eminramos16@gmail.com>
2023-11-27 11:43:15 -0600

    feat(Printers): :sparkles: Feature para exportar datos en un archivo de excel

--- app/Filament/Resources/PrinterResource/Pages/ListPrinters.php
+++ app/Filament/Resources/PrinterResource/Pages/ListPrinters.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\PrinterResource\Pages;
 use App\Filament\Resources\PrinterResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Columns\Column;
 
 class ListPrinters extends ListRecords
 {
@@ -14,6 +17,22 @@ class ListPrinters extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+
+            ExportAction::make()
+                ->exports([
+                    ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename("Impresoras" . '-' . date('Y-m-d'))
+                        ->withWriterType(\Maatwebsite\Excel\Excel::XLSX)
+                        ->withColumns([
+                            Column::make('brand.name')->heading('Marca'),
+                            Column::make('model.name')->heading('Modelo'),
+                            Column::make('type.name')->heading('Tipo'),
+                            Column::make('description')->heading('Descripción'),
+                            Column::make('condition')->heading('Condición'),
+                            Column::make('entry_date')->heading('Fecha de Ingreso'),
+                        ]),
+                ]),
         ];
     }
 }
