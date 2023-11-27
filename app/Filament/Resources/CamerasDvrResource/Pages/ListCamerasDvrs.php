3cc3e882bd05940303379e133a3d4e72b78447cc
EminRams <eminramos16@gmail.com>
2023-11-27 10:42:24 -0600

    feat(Cameras_DVR's): :bug: Correcion en la feature exportar datos en excel, no se mostraban todas las columnas

--- app/Filament/Resources/CamerasDvrResource/Pages/ListCamerasDvrs.php
+++ app/Filament/Resources/CamerasDvrResource/Pages/ListCamerasDvrs.php
@@ -7,6 +7,7 @@ use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
 use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Columns\Column;
 
 class ListCamerasDvrs extends ListRecords
 {
@@ -22,7 +23,14 @@ class ListCamerasDvrs extends ListRecords
                 ExcelExport::make()
                     ->fromTable()
                     ->withFilename("Camaras-Dvr's" . '-' . date('Y-m-d'))
-                    ->withWriterType(\Maatwebsite\Excel\Excel::XLSX),
+                    ->withWriterType(\Maatwebsite\Excel\Excel::XLSX)
+                    ->withColumns([
+                        Column::make('dvr_program')->heading('Programa de Dvr'),
+                        Column::make('condition')->heading('Condición'),
+                        Column::make('enrty_date')->heading('Fecha de Ingreso'),
+                        Column::make('observation')->heading('Observación'),
+                        Column::make('state')->heading('Estado'),
+                    ]),
             ]),
         ];
     }
